c8a0aebee0f0fb3b0f2ea9a9c682dfc9a909522e
Faisal <faisal.j@ufaber.com>
2020-11-18 17:26:52 +0530

    Initial commit

---
+++ lib/UI/main_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:restaurant_finder/BLoC/bloc_provider.dart';
+import 'package:restaurant_finder/BLoC/location_bloc.dart';
+import 'package:restaurant_finder/DataLayer/location.dart';
+
+import 'location_screen.dart';
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<Location>(
+      stream: BlocProvider.of<LocationBloc>(context).locationStream,
+      builder: (context, snapshot) {
+        final location = snapshot.data;
+
+        // 2
+        if (location == null) {
+          return LocationScreen();
+        }
+
+        // This will be changed this later
+        return Container();
+      },
+    );
+  }
+}
